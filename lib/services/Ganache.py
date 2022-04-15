bae6c3df9f9c4e5247fb194ea947a54c1555729e
gabrielruoff <geruoff@gmail.com>
2022-04-15 04:31:04 +0000

    standardized stop method with _detect_running()

--- lib/services/Ganache.py
+++ lib/services/Ganache.py
@@ -21,6 +21,17 @@ class Ganache:
         _executable = os.environ.get('GANACHE_EXECUTABLE')
         _executable = subprocess.check_output(['bash', 'which', _executable])
         self.executable = _executable.decode("utf-8").rstrip('\n')
+        self.pid = None
+
+    def _detect_running(self):
+        try:
+            # get pid of ganache process
+            self.pid = subprocess.check_output(['lsof', '-t', '-i:' + self.port]).decode("utf-8").rstrip('\n')
+            logging.debug('Ganache is running ({})'.format(self.pid))
+            return True
+        except subprocess.CalledProcessError as e:
+            logging.debug('Ganache is not running')
+        return False
 
     def start(self):
         # --db flag sets datadir
@@ -30,17 +41,20 @@ class Ganache:
         logging.debug('started Ganache')
 
     def stop(self):
-        try:
-            # get pid of ganache process
-            _pid = subprocess.check_output(['lsof', '-t', '-i:' + self.port]).decode("utf-8").rstrip('\n')
-            _stopcommand = ['kill', '-9', _pid]
-            # kill ganache process
-            subprocess.check_output(_stopcommand).decode("utf-8")
-            logging.debug('stopped Ganache')
-        except subprocess.CalledProcessError as e:
-            logging.debug('Ganache failed to stop or is not started: {}'.format(e))
+        logging.debug('Stopping Ganache')
+        if self._detect_running():
+            try:
+                _stopcommand = ['kill', '-9', self.pid]
+                # kill ganache process
+                subprocess.check_output(_stopcommand).decode("utf-8")
+                logging.debug('stopped Ganache ({})'.format(self.pid))
+            except subprocess.CalledProcessError as e:
+                 logging.debug('Ganache failed to stop or is not started: {}'.format(e))
+        else:
+            logging.debug('Cannot stop. Ganache is not running')
 
 # g = Ganache()
+# g.stop()
 # g.start()
 # time.sleep(15)
 # g.stop()
