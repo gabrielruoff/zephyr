7000ccdeaddd2a1c735f4a4b4bce753fa9ef99fd
gabrielruoff <geruoff@gmail.com>
2022-04-15 09:56:59 +0000

    added mysql password

---
+++ test.py
@@ -0,0 +1,11 @@
+import logging
+from lib.core import initexit
+from dotenv import load_dotenv
+
+load_dotenv()
+logging.basicConfig(filename='/home/common/dev/logs/zephyr.log', level=logging.DEBUG)
+
+initexithandler = initexit.initexit()
+initexithandler.import_dependencies()
+initexithandler.exit()
+initexithandler.init()
