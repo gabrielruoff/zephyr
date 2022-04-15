222bdaac5d461ec1f5d0b617f3f6452d746f1001
gabrielruoff <geruoff@gmail.com>
2022-04-15 00:07:00 +0000

    created Ganache.py interface between python and ganache-cli

---
+++ lib/services/Ganache.py
@@ -0,0 +1,39 @@
+import subprocess
+import logging
+import os
+from dotenv import load_dotenv
+
+load_dotenv('/home/common/dev/.env')
+
+
+# Ganache local ethereum blockchain
+class Ganache:
+    def __init__(self):
+        logging.debug('initializing Ganache')
+        # get ganache data dir from .env
+        self.datadir = os.environ.get('GANACHE_DATADIR')
+        # port that ganache runs on
+        self.port = os.environ.get('GANACHE_PORT')
+        # get absolute path of executable
+        _executable = os.environ.get('GANACHE_EXECUTABLE')
+        _executable = subprocess.check_output(['bash', 'which', _executable])
+        self.executable = _executable.decode("utf-8").rstrip('\n')
+
+    def start(self):
+        # --db flag sets datadir
+        _startcommand = [self.executable, '--db', self.datadir]
+        # start ganache and detach
+        subprocess.Popen(_startcommand, start_new_session=True)
+        logging.debug('started Ganache')
+
+    def stop(self):
+        try:
+            # get pid of ganache process
+            _pid = subprocess.check_output(['lsof', '-t', '-i:' + self.port]).decode("utf-8").rstrip('\n')
+            _stopcommand = ['kill', '-9', _pid]
+            # kill ganache process
+            subprocess.check_output(_stopcommand).decode("utf-8")
+            logging.debug('stopped Ganache')
+        except subprocess.CalledProcessError as e:
+            logging.debug('Ganache failed to stop or is not started: {}'.format(e))
+
