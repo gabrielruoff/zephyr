707162feca344432830abd1d1cfcd11826f7b316
gabrielruoff <geruoff@gmail.com>
2022-04-15 10:25:35 +0000

    fixed sourcing of mysql password from .env

--- lib/services/Ledger.py
+++ lib/services/Ledger.py
@@ -19,6 +19,7 @@ class Ledger:
         self.containername = 'ledger'
         self.imagename = os.environ.get('LEDGER_DOCKER_IMAGE')
         self.port = os.environ.get('LEDGER_PORT')
+        self.passwd = os.environ.get('LEDGER_PASS')
         self.ledger = None
         self._detect_running()
 
@@ -42,7 +43,7 @@ class Ledger:
         logging.debug('starting ledger')
         _volumes = ['{}:{}'.format(self.confdir, '/etc/mysql'), '{}:{}'.format(self.datadir, '/var/lib/mysql')]
         _ports = {'3306/tcp': int(self.port)}
-        _environment = ['MYSQL_ROOT_PASSWORD=zephyr']
+        _environment = ['MYSQL_ROOT_PASSWORD={}'.format(self.passwd)]
         if not self._detect_running():
             try:
                 self.ledger = self.docker.containers.run(self.imagename, name=self.containername,
