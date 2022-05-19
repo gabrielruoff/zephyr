4e8a3cfbb9fcdac0febd563a2214d6920e94d12b
gabrielruoff <geruoff@gmail.com>
2022-05-19 03:22:21 -0400

    added old card reader firmware and test scripts

---
+++ Firmware/Scripts/readDataFromCard.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+from lib.Crypt import RSAcrypt
+from lib.HardwareSerial import arduino
+import time
+
+# load .env
+load_dotenv()
+# set env variables
+DATADIR = os.environ.get("DATADIR")
+MASTER_KEY_PREF = os.environ.get("MASTER_KEY_PREF")
+MASTER_KEY_DIR = os.environ.get("MASTER_KEY_DIR")
+KEY_PRIV_SUFFIX = os.environ.get("KEY_PRIV_SUFFIX")
+MASTER_KEY_PASS = os.environ.get("MASTER_KEY_PASS")
+
+rsacrypt = RSAcrypt()
+rsacrypt.set_key(DATADIR+MASTER_KEY_DIR+MASTER_KEY_PREF+KEY_PRIV_SUFFIX, MASTER_KEY_PASS)
+arduino = arduino()
+
+arduino.connect("COM3")
+print('connecting to arduino')
+time.sleep(3)
+
+arduino.send('r')
+recv = arduino.receive()
+recv = rsacrypt.decrypt(recv)
+
+print("decrypted account identifier: "+recv)
+
+# while(1):
+#
+#     if(arduino.serialport.in_waiting):
+#         print(arduino.serialport.readline())
