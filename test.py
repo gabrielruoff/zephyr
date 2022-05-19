ecbc76cedb5525f5e0071fb3e1b1b2209ad4d1e5
gabrielruoff <geruoff@gmail.com>
2022-05-19 07:18:29 +0000

    ledger transaction tests

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import logging
+import sys
+import time
 
 from lib.core import initexit
 from dotenv import load_dotenv
@@ -8,5 +10,15 @@ logging.basicConfig(filename='/home/common/dev/logs/zephyr.log', level=logging.D
 
 initexithandler = initexit.initexit()
 initexithandler.import_dependencies()
-initexithandler.exit()
-initexithandler.init()
+# initexithandler.exit()
+# time.sleep(1)
+# initexithandler.init()
+# time.sleep(2)
+
+c = initexithandler.core['LedgerClient'].LedgerClient()
+# c._mysql_update('transactions',['value'],['10'],['tx', 'tx'])
+# c.do_ledger_transaction('tx', 'rx', '10')
+# c._change_balance('1', '10')
+print(c._get_user_id_from_username('test'))
+print(c._get_balance(username='test'))
+c.do_ledger_transaction(c._get_user_id_from_username('bank'), c._get_user_id_from_username('test'), 10)
