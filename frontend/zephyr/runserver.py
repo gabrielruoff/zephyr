3ee77a1f6fa83b2b522cd3f49f37d944d49872fe
gabrielruoff <geruoff@gmail.com>
2022-05-24 19:16:28 +0000

    changed host to 0.0.0.0

--- frontend/zephyr/runserver.py
+++ frontend/zephyr/runserver.py
@@ -1,7 +1,7 @@
 import subprocess
 import manage
 
-startcommand = ['/home/common/dev/venv/bin/python3.8', 'manage.py', 'runserver']
+startcommand = ['/home/common/dev/venv/bin/python3.8', 'manage.py', 'runserver', '0.0.0.0:8000']
 # start ganache and detach process
 sp = subprocess.Popen(startcommand)#, start_new_session=True)
 out, err = sp.communicate()
