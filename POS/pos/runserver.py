b2c30d652483d0878eebc6314a26f9d5215c73cf
Gabriel Ruoff <geruoff@gmail.com>
2022-05-27 17:36:15 -0400

    added support for multiple os's

--- POS/pos/runserver.py
+++ POS/pos/runserver.py
@@ -1,8 +1,18 @@
+import platform
 import subprocess
 import os
 
 certargs = []
-startcommand = ['{}\\Scripts\\python.exe'.format(os.environ['VIRTUAL_ENV']), 'manage.py', 'runserver', '127.0.0.1:8000']
+if platform.system() == "linux" or platform.system() == "linux2":
+    # linux
+    pass
+elif platform.system() == "Darwin":
+    # OS X
+    exe = '{}/bin/python'.format(os.environ['VIRTUAL_ENV'])
+elif platform.system() == "win32":
+    exe = '{}\\Scripts\\python.exe'.format(os.environ['VIRTUAL_ENV'])
+
+startcommand = [exe, 'manage.py', 'runserver', '127.0.0.1:8000']
 # start ganache and detach process
 sp = subprocess.Popen(startcommand)#, start_new_session=True)
 out, err = sp.communicate()
